e1ca0a3461076afdd1963b370de914d4bb052000
seo <gogo392539@gmail.com>
2018-06-27 20:37:23 +0900

    mod winform

--- windowForm/windowForm/mywin.cpp
+++ windowForm/windowForm/mywin.cpp
@@ -4,9 +4,16 @@ C_MYWIN* C_MYWIN::m_pMyWin = nullptr;
 
 C_MYWIN::C_MYWIN() :
 	m_hInstance(NULL),
-	m_hWnd(NULL),
-	m_hWnd2(NULL),
-	m_hBtn(NULL)
+	m_hWndLogin(NULL),
+	m_hWndJoin(NULL),
+	m_hWndComm(NULL),
+	m_nWinHeight(0),
+	m_nWinPosX(0),
+	m_nWinPosY(0),
+	m_nWinWidth(0),
+	m_cLoginInfo(),
+	m_cJoinInfo(),
+	m_cCommInfo()
 {
 }
 
@@ -30,30 +37,57 @@ void C_MYWIN::releaseWin()
 	}
 }
 
-LRESULT C_MYWIN::wndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+LRESULT C_MYWIN::wndLoginProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
-	return m_pMyWin->myProc(hWnd, message, wParam, lParam);
+	return m_pMyWin->myLoginProc(hWnd, message, wParam, lParam);
 }
 
-LRESULT C_MYWIN::myProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+LRESULT C_MYWIN::myLoginProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
 	switch (message)
 	{
 	case WM_CREATE:
-		CreateWindow(TEXT("button"), L"WIN2 HIDE", WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON, 100, 100, 100, 20, hWnd, (HMENU)IDC_BUTTON1, m_hInstance, NULL);
-		CreateWindow(TEXT("button"), L"ALL SHOW", WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON, 100, 400, 100, 20, hWnd, (HMENU)IDC_BUTTON3, m_hInstance, NULL);
-		
+		m_cLoginInfo.initLoginControl(hWnd, m_hInstance);
+		break;
+	case WM_SIZING:
+	{
+		RECT * pRectSize = (RECT*)lParam;
+		MoveWindow(m_hWndJoin, pRectSize->left, pRectSize->top, pRectSize->right - pRectSize->left, pRectSize->bottom - pRectSize->top, false);
+		MoveWindow(m_hWndComm, pRectSize->left, pRectSize->top, pRectSize->right - pRectSize->left, pRectSize->bottom - pRectSize->top, false);
+	}
+		break;
+	case WM_MOVING:
+	{
+		RECT * pRectMove = (RECT*)lParam;
+		MoveWindow(m_hWndJoin, pRectMove->left, pRectMove->top, pRectMove->right - pRectMove->left, pRectMove->bottom - pRectMove->top, false);
+		MoveWindow(m_hWndComm, pRectMove->left, pRectMove->top, pRectMove->right - pRectMove->left, pRectMove->bottom - pRectMove->top, false);
+	}
 		break;
 	case WM_COMMAND:
 		switch (GET_X_LPARAM(wParam))
 		{
-		case IDC_BUTTON1:
-			ShowWindow(m_hWnd2, SW_HIDE);
+		case IDC_LOGIN_BTS_OK:
+		{
+			m_cLoginInfo.getEditText();
+
 			
+
+			//로그인 network 추가
+
+			m_cLoginInfo.initEditText();
+
+			ShowWindow(m_hWndComm, SW_SHOWDEFAULT);
+			ShowWindow(hWnd, SW_HIDE);
+		}
 			break;
-		case IDC_BUTTON3:
-			ShowWindow(hWnd, SW_SHOWDEFAULT);
-			ShowWindow(m_hWnd2, SW_SHOWDEFAULT);
+		case IDC_LOGIN_BTS_CANCEL:
+			DestroyWindow(hWnd);
+			break;
+		case IDC_LOGIN_BTS_JOIN:
+			m_cLoginInfo.initEditText();
+
+			ShowWindow(m_hWndJoin, SW_SHOWDEFAULT);
+			ShowWindow(hWnd, SW_HIDE);
 			break;
 		}
 		break;
@@ -74,30 +108,148 @@ LRESULT C_MYWIN::myProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 	return 0;
 }
 
-LRESULT C_MYWIN::wndProc2(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+LRESULT C_MYWIN::wndJoinProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
-	return m_pMyWin->myProc2(hWnd, message, wParam, lParam);
+	return m_pMyWin->myJoinProc(hWnd, message, wParam, lParam);
 }
 
-LRESULT C_MYWIN::myProc2(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+LRESULT C_MYWIN::myJoinProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
 	switch (message)
 	{
 	case WM_CREATE:
-		CreateWindow(TEXT("button"), L"WIN1 HIDE", WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON, 100, 100, 100, 20, hWnd, (HMENU)IDC_BUTTON2, m_hInstance, NULL);
-		CreateWindow(TEXT("button"), L"ALL SHOW", WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON, 100, 400, 100, 20, hWnd, (HMENU)IDC_BUTTON3, m_hInstance, NULL);
+		m_cJoinInfo.initJoinControl(hWnd, m_hInstance);
 
 		break;
+	case WM_SIZING:
+	{
+		RECT * pRectSize = (RECT*)lParam;
+		MoveWindow(m_hWndLogin, pRectSize->left, pRectSize->top, pRectSize->right - pRectSize->left, pRectSize->bottom - pRectSize->top, false);
+		MoveWindow(m_hWndComm, pRectSize->left, pRectSize->top, pRectSize->right - pRectSize->left, pRectSize->bottom - pRectSize->top, false);
+	}
+	break;
+	case WM_MOVING:
+	{
+		RECT * pRectMove = (RECT*)lParam;
+		MoveWindow(m_hWndLogin, pRectMove->left, pRectMove->top, pRectMove->right - pRectMove->left, pRectMove->bottom - pRectMove->top, false);
+		MoveWindow(m_hWndComm, pRectMove->left, pRectMove->top, pRectMove->right - pRectMove->left, pRectMove->bottom - pRectMove->top, false);
+	}
+	break;
 	case WM_COMMAND:
 		switch (GET_X_LPARAM(wParam))
 		{
-		case IDC_BUTTON2:
-			ShowWindow(m_hWnd, SW_HIDE);
+		case IDC_JOIN_BTS_OK:
+		{
+			if (!m_cJoinInfo.idOverlapCheck())
+			{
+				MessageBox(hWnd, L"ID 중복 확인 하세요", L"회원가입 실패", MB_OK);
+				break;
+			}
+			else
+			{
+				if (!m_cJoinInfo.NickOverlapCheck())
+				{
+					MessageBox(hWnd, L"Nick Name 중복 확인 하세요", L"회원가입 실패", MB_OK);
+					break;
+				}
+			}
+
+			m_cJoinInfo.getEditText();
+
+			if (!m_cJoinInfo.pwEqualCheck())
+			{
+				MessageBox(hWnd, L"Password 확인 틀림", L"회원가입 실패", MB_OK);
+				break;
+			}
+
+			//회원가입 network
+
+
+			m_cJoinInfo.initEditText();
+
+			MessageBox(hWnd, L"회원가입 완료", L"회원가입", MB_OK);
+			
+			ShowWindow(hWnd, SW_HIDE);
+			ShowWindow(m_hWndLogin, SW_SHOWDEFAULT);
+		}
+			break;
+		case IDC_JOIN_BTS_CANCEL:
+			m_cJoinInfo.initEditText();
+
+			ShowWindow(hWnd, SW_HIDE);
+			ShowWindow(m_hWndLogin, SW_SHOWDEFAULT);
+			break;
+		case IDC_JOIN_BTS_ID_OVERLAP:
+			//ID 중복 확인 network
 
 			break;
-		case IDC_BUTTON3:
-			ShowWindow(hWnd, SW_SHOWDEFAULT);
-			ShowWindow(m_hWnd, SW_SHOWDEFAULT);
+		case IDC_JOIN_BTS_NICK_OVERLAP:
+			//NICKNAME 중복 확인 network
+
+			break;
+		}
+		break;
+	case WM_PAINT:
+	{
+		PAINTSTRUCT ps;
+		HDC hdc = BeginPaint(hWnd, &ps);
+		// TODO: 여기에 hdc를 사용하는 그리기 코드를 추가합니다.
+		EndPaint(hWnd, &ps);
+	}
+	break;
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		break;
+	default:
+		return DefWindowProc(hWnd, message, wParam, lParam);
+	}
+	return 0;
+}
+
+LRESULT C_MYWIN::wndCommProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	return m_pMyWin->myCommProc(hWnd, message, wParam, lParam);
+}
+
+LRESULT C_MYWIN::myCommProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	switch (message)
+	{
+	case WM_CREATE:
+		m_cCommInfo.initCommControl(hWnd, m_hInstance);
+		break;
+	case WM_SIZING:
+	{
+		RECT * pRectSize = (RECT*)lParam;
+		MoveWindow(m_hWndLogin, pRectSize->left, pRectSize->top, pRectSize->right - pRectSize->left, pRectSize->bottom - pRectSize->top, false);
+		MoveWindow(m_hWndJoin, pRectSize->left, pRectSize->top, pRectSize->right - pRectSize->left, pRectSize->bottom - pRectSize->top, false);
+	}
+	break;
+	case WM_MOVING:
+	{
+		RECT * pRectMove = (RECT*)lParam;
+		MoveWindow(m_hWndLogin, pRectMove->left, pRectMove->top, pRectMove->right - pRectMove->left, pRectMove->bottom - pRectMove->top, false);
+		MoveWindow(m_hWndJoin, pRectMove->left, pRectMove->top, pRectMove->right - pRectMove->left, pRectMove->bottom - pRectMove->top, false);
+	}
+	break;
+	case WM_COMMAND:
+		switch (GET_X_LPARAM(wParam))
+		{
+		case IDC_COMM_BTS_OK:
+			m_cCommInfo.transInputText();
+			break;
+		case IDC_COMM_BTS_CANCEL:
+			m_cCommInfo.initEditText();
+			//voice network
+
+			ShowWindow(hWnd, SW_HIDE);
+			ShowWindow(m_hWndLogin, SW_SHOWDEFAULT);
+			break;
+		case IDC_COMM_BTS_VOICE_OK:
+			//voice network
+			break;
+		case IDC_COMM_BTS_VOICE_CANCEL:
+			//voice network
 			break;
 		}
 		break;
@@ -120,45 +272,66 @@ LRESULT C_MYWIN::myProc2(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 
 bool C_MYWIN::init(HINSTANCE hInstance)
 {
-	WNDCLASSEXW wcex;
-	wcex.cbSize = sizeof(WNDCLASSEX);
-	wcex.style = CS_HREDRAW | CS_VREDRAW;
-	wcex.lpfnWndProc = wndProc;
-	wcex.cbClsExtra = 0;
-	wcex.cbWndExtra = 0;
-	wcex.hInstance = hInstance;
-	wcex.hIcon = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_WINDOWFORM));
-	wcex.hCursor = LoadCursor(nullptr, IDC_ARROW);
-	wcex.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
-	wcex.lpszMenuName = 0;
-	wcex.lpszClassName = L"winForm";
-	wcex.hIconSm = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
-	RegisterClassExW(&wcex);
+	WNDCLASSEXW wcexLogin;
+	wcexLogin.cbSize = sizeof(WNDCLASSEX);
+	wcexLogin.style = CS_HREDRAW | CS_VREDRAW;
+	wcexLogin.lpfnWndProc = wndLoginProc;
+	wcexLogin.cbClsExtra = 0;
+	wcexLogin.cbWndExtra = 0;
+	wcexLogin.hInstance = hInstance;
+	wcexLogin.hIcon = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_WINDOWFORM));
+	wcexLogin.hCursor = LoadCursor(nullptr, IDC_ARROW);
+	wcexLogin.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+	wcexLogin.lpszMenuName = 0;
+	wcexLogin.lpszClassName = L"Login";
+	wcexLogin.hIconSm = LoadIcon(wcexLogin.hInstance, MAKEINTRESOURCE(IDI_SMALL));
+	RegisterClassExW(&wcexLogin);
 
-	WNDCLASSEXW wcex2;
-	wcex2 = wcex;
-	wcex2.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
-	wcex2.lpszClassName = L"winForm2";
-	wcex2.lpfnWndProc = wndProc2;
-	RegisterClassExW(&wcex2);
+	WNDCLASSEXW wcexJoin;
+	wcexJoin = wcexLogin;
+	wcexJoin.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+	wcexJoin.lpszClassName = L"Join";
+	wcexJoin.lpfnWndProc = wndJoinProc;
+	RegisterClassExW(&wcexJoin);
 
-	m_hWnd = CreateWindowW(L"winForm", nullptr, WS_OVERLAPPEDWINDOW,
-		CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, nullptr, nullptr, hInstance, nullptr);
-	if (!m_hWnd)
+	WNDCLASSEXW wcexComm;
+	wcexComm = wcexLogin;
+	wcexComm.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+	wcexComm.lpszClassName = L"Communication";
+	wcexComm.lpfnWndProc = wndCommProc;
+	RegisterClassExW(&wcexComm);
+
+
+	m_nWinPosX = 200;
+	m_nWinPosY = 200;
+	m_nWinHeight = 500;
+	m_nWinWidth = 500;
+
+	m_hWndLogin = CreateWindowW(L"Login", L"Login", WS_OVERLAPPEDWINDOW,
+		m_nWinPosX, m_nWinPosY, m_nWinWidth, m_nWinHeight, nullptr, nullptr, hInstance, nullptr);
+	if (!m_hWndLogin)
 		return false;
 
-	m_hWnd2 = CreateWindowW(L"winForm2", nullptr, WS_OVERLAPPEDWINDOW,
-		CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, nullptr, nullptr, hInstance, nullptr);
-	if (!m_hWnd2)
+	m_hWndJoin = CreateWindowW(L"Join", L"Join", WS_OVERLAPPEDWINDOW,
+		m_nWinPosX, m_nWinPosY, m_nWinWidth, m_nWinHeight, nullptr, nullptr, hInstance, nullptr);
+	if (!m_hWndJoin)
+		return false;
+
+	m_hWndComm = CreateWindowW(L"Communication", L"Communication", WS_OVERLAPPEDWINDOW,
+		m_nWinPosX, m_nWinPosY, m_nWinWidth, m_nWinHeight, nullptr, nullptr, hInstance, nullptr);
+	if (!m_hWndComm)
 		return false;
 
 	m_hInstance = hInstance;
 
-	ShowWindow(m_hWnd, SW_SHOWDEFAULT);
-	UpdateWindow(m_hWnd);
+	ShowWindow(m_hWndLogin, SW_SHOWDEFAULT);
+	UpdateWindow(m_hWndLogin);
 
-	ShowWindow(m_hWnd2, SW_SHOWDEFAULT);
-	UpdateWindow(m_hWnd2);
+	ShowWindow(m_hWndJoin, SW_HIDE);
+	UpdateWindow(m_hWndJoin);
+
+	ShowWindow(m_hWndComm, SW_HIDE);
+	UpdateWindow(m_hWndComm);
 
 	return true;
 }
--- windowForm/windowForm/mywin.h
+++ windowForm/windowForm/mywin.h
@@ -1,15 +1,27 @@
 #pragma once
 
 #include <Windows.h>
+#include "loginInfo.h"
+#include "joinInfo.h"
+#include "commInfo.h"
 
 class C_MYWIN
 {
 private:
 	HINSTANCE	m_hInstance;
-	HWND		m_hWnd;
-	HWND		m_hWnd2;
+	HWND		m_hWndLogin;
+	HWND		m_hWndJoin;
+	HWND		m_hWndComm;
 
-	HWND		m_hBtn;
+	int			m_nWinPosX;
+	int			m_nWinPosY;
+	int			m_nWinHeight;
+	int			m_nWinWidth;
+
+private:
+	C_LOGININFO m_cLoginInfo;
+	C_JOININFO m_cJoinInfo;
+	C_COMMINFO m_cCommInfo;
 
 private:
 	static C_MYWIN* m_pMyWin;
@@ -21,10 +33,12 @@ public:
 
 private:
 	C_MYWIN();
-	static LRESULT CALLBACK wndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam);
-	LRESULT CALLBACK myProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam);
-	static LRESULT CALLBACK wndProc2(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam);
-	LRESULT CALLBACK myProc2(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam);
+	static LRESULT CALLBACK wndLoginProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam);
+	LRESULT CALLBACK myLoginProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam);
+	static LRESULT CALLBACK wndJoinProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam);
+	LRESULT CALLBACK myJoinProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam);
+	static LRESULT CALLBACK wndCommProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam);
+	LRESULT CALLBACK myCommProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam);
 
 public:
 	bool init(HINSTANCE hInstance);
